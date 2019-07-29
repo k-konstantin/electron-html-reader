fd05165f4191639740f240d3651fc0f8a455fb0c
Konstantin Kuznetsov <kuznetsovky@gmail.com>
2019-07-29 16:13:13 +0300

    v0.0.4. Simple React UI.

--- src/renderer/components/MainPage.js
+++ src/renderer/components/MainPage.js
@@ -5,48 +5,48 @@ class MainPage extends Component {
         left: 0,
         top: 0,
         transform: '',
-    }
+    };
     constructor(props) {
-        super(props)
-        this.iframeContainerRef = React.createRef()
+        super(props);
+        this.iframeContainerRef = React.createRef();
         this.onKeyUp = this.onKeyUp.bind(this);
     }
     componentDidMount() {
-        window.addEventListener('resize', this.onWindowResize)  
-        document.addEventListener('keydown', this.onKeyUp)
-        this.onWindowResize() 
+        window.addEventListener('resize', this.onWindowResize);
+        document.addEventListener('keydown', this.onKeyUp);
+        this.onWindowResize();
     }
     componentWillUnmount() {
-        window.removeEventListener('resize', this.onWindowResize) 
-        document.removeEventListener('keydown', this.onKeyUp)
+        window.removeEventListener('resize', this.onWindowResize);
+        document.removeEventListener('keydown', this.onKeyUp);
     }
     onKeyUp(event) {
         if (event.key === 'ArrowRight') {
-            this.props.nextAnimation()
+            this.props.nextAnimation();
         } else if (event.key === 'ArrowLeft') {
-            this.props.prevAnimation()
+            this.props.prevAnimation();
         }
     }
     onWindowResize = () => {
-        this.setState(this.getContentStyle())
-    }
+        this.setState(this.getContentStyle());
+    };
     getContentStyle = () => {
         const WIDTH = 1280;
         const HEIGHT = 800;
 
-        const containerRef = this.iframeContainerRef.current
-        const scaleX = containerRef.offsetWidth / WIDTH
-        const scaleY = containerRef.offsetHeight / HEIGHT
-        const scale = Math.min(scaleX, scaleY)
-        const left = scaleY < scaleX ? (containerRef.offsetWidth - WIDTH * scale) * 0.5 : 0
-        const top = scaleX < scaleY ? (containerRef.offsetHeight - HEIGHT * scale) * 0.5 : 0
+        const containerRef = this.iframeContainerRef.current;
+        const scaleX = containerRef.offsetWidth / WIDTH;
+        const scaleY = containerRef.offsetHeight / HEIGHT;
+        const scale = Math.min(scaleX, scaleY);
+        const left = scaleY < scaleX ? (containerRef.offsetWidth - WIDTH * scale) * 0.5 : 0;
+        const top = scaleX < scaleY ? (containerRef.offsetHeight - HEIGHT * scale) * 0.5 : 0;
 
         return {
             transform: `scale(${scale})`,
             left,
             top,
-        }
-    }
+        };
+    };
     render() {
         const { animations, selectedId, isFetching, selectAnimation, openFolder } = this.props;
 
@@ -58,28 +58,22 @@ class MainPage extends Component {
                         <div>Loading</div>
                     ) : (
                         animations.map((path, index) => (
-                            <div 
-                                key={path}
-                                title={path}
-                                className={index === selectedId ? 'btn selected' : 'btn'}
-                                onClick={() => selectAnimation(index)}
-                            >
-                                {path.slice(-30)}
+                            <div key={path} title={path} className={index === selectedId ? 'btn selected' : 'btn'} onClick={() => selectAnimation(index)}>
+                                {path.slice(path.lastIndexOf('\\') + 1)}
                             </div>
                         ))
                     )}
                 </div>
                 <div className='content-container'>
-                    <div className='content-title' title={animations[selectedId]}>{animations[selectedId] && animations[selectedId].slice(-50) || 'Untitled'}</div>
+                    <div className='content-title' title={animations[selectedId]}>
+                        {(animations[selectedId] && animations[selectedId].slice(animations[selectedId].lastIndexOf('\\') + 1)) || 'Untitled'}
+                    </div>
                     <div ref={this.iframeContainerRef} className='iframe-container'>
-                        <iframe 
-                            src={animations[selectedId]}
-                            style={this.state}
-                        />
+                        <iframe src={animations[selectedId]} style={this.state} />
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
