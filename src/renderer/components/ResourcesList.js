fdc48326c4b6678c653837016a534cc05e3c0ddd
Konstantin Kuznetsov <kuznetsovky@gmail.com>
2020-07-31 11:35:36 +0300

    In ResourcesList.js removed redundant nesting.

--- src/renderer/components/ResourcesList.js
+++ src/renderer/components/ResourcesList.js
@@ -18,25 +18,23 @@ const ResourcesList = ({
     return (
         <List dense classes={{root: styles.root}}>
             {animations.map((path, index) => (
-                <>
-                    <ListItem 
-                        key={path} 
-                        button
-                        divider
-                        selected={selectedId === index}
-                        title={path} 
-                        onClick={onListItemClick(index)}
-                    >
-                        <ListItemText
-                            primary={getFileNameFromSrc(path)}
-                        />
-                        <ListItemSecondaryAction>
-                            <IconButton title={'Открыть в проводнике'} onClick={onRevealInExplorer(path)} edge="end">
-                                <FolderOpen />
-                            </IconButton>
-                        </ListItemSecondaryAction>
-                    </ListItem>
-                </>
+                <ListItem 
+                    key={path} 
+                    button
+                    divider
+                    selected={selectedId === index}
+                    title={path} 
+                    onClick={onListItemClick(index)}
+                >
+                    <ListItemText
+                        primary={getFileNameFromSrc(path)}
+                    />
+                    <ListItemSecondaryAction>
+                        <IconButton title={'Открыть в проводнике'} onClick={onRevealInExplorer(path)} edge="end">
+                            <FolderOpen />
+                        </IconButton>
+                    </ListItemSecondaryAction>
+                </ListItem>
             ))}
         </List>
     )
